06c56c68940c4e1f37dbee77988681aa1ed8abe2
skolden1 <skolden123@hotmail.com>
2025-04-04 09:34:28 +0200

    Added cart tbl, updated ordertbl

---
+++ CrownGardenRazor/Migrations/AppDb/20250404072213_UpdatedOrdertblAddedCartTbl.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrownGardenRazor.Migrations.AppDb
+{
+    /// <inheritdoc />
+    public partial class UpdatedOrdertblAddedCartTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    CartId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.CartId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ CrownGardenRazor/Model/Cart.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+
+namespace CrownGardenRazor.Model
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+        public string UserId { get; set; } //för o koppla till identity då den är string
+
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
