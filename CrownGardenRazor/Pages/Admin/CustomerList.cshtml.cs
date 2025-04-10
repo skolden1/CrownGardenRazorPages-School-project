74b67051b5a4f9cfe802d0e23aed77be1f414638
YoussefKouliana <yosep.koliana@gmail.com>
2025-04-10 08:54:54 +0200

    adding filter to customer list

--- CrownGardenRazor/Pages/Admin/CustomerList.cshtml.cs
+++ CrownGardenRazor/Pages/Admin/CustomerList.cshtml.cs
@@ -1,13 +1,15 @@
+
 using CrownGardenRazor.Areas.Identity.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 
 namespace CrownGardenRazor.Pages.Admin
 {
-   // [Authorize(Roles = "Admin")] 
+    // [Authorize(Roles = "Admin")] 
     public class CustomerListModel : PageModel
     {
         private readonly UserManager<IdentityUserTable> _userManager;
@@ -19,9 +21,40 @@ namespace CrownGardenRazor.Pages.Admin
 
         public List<IdentityUserTable> Customers { get; set; }
 
+
+
+
+
+        [BindProperty(SupportsGet = true)]
+        public string Search { get; set; } = string.Empty;
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchFilter { get; set; } = "name";
+
         public async Task OnGetAsync()
         {
-            Customers = await _userManager.Users.ToListAsync();
+            var users = _userManager.Users.AsQueryable();
+
+
+            if (!string.IsNullOrEmpty(Search))
+            {
+                if (SearchFilter == "membership")
+                {
+                    users = users.Where(c => c.MembershipLevel != null && c.MembershipLevel.Contains(Search));
+                }
+
+
+
+                else
+                {
+                    users = users.Where(c =>
+                    (c.FirstName != null && c.FirstName.Contains(Search)) ||
+                    (c.LastName != null && c.LastName.Contains(Search)));
+                }
+            }
+            Customers = await users.ToListAsync();
+
         }
     }
+
 }
